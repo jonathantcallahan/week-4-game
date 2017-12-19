6286034b8a642938cb13837b14c688bf36754782
unknown <jtc7537@gmail.com>
2017-12-18 22:20:13 -0500

    retrieved character stats using ids from clicks

--- starwars.js
+++ starwars.js
@@ -3,66 +3,204 @@ var defenderChosen = false;
 var clickIterator = 0;
 var selectedAttacker;
 var selectedDefender;
-
-//	character1 = {
-//		name: "Obi Wan",
-//		health: 100,
-//		attack: 20,
-//		Defender: false,
-//		Attacker: false,
-//	}
-//	
-//	character2 = {
-//		name: "Darth Vader",
-//		health: 100,
-//		attack: 20,
-//		Defender: false,
-//		Attacker: false,
-//	}
-//	
-//	character3 = {
-//		name: "Luke Skywalker",
-//		health: 100,
-//		attack: 20,
-//		Defender: false,
-//		Attacker: false,
-//	}
-//	
-//	
-//	character4 = {
-//		name: "Mace Windu",
-//		health: 100,
-//		attack: 20,
-//		Defender: false,
-//		Attacker: false,
-//	}
-var CharacterOneHealth = 130
 var playerHealth;
+var playerAttack;
+var defenderHealth;
+var defenderAttack;
+var wins = 0;
+
+character1 = {
+	name: "Obi Wan",
+	health: 100,
+	attack: 20,
+	Defender: false,
+	Attacker: false,
+	setStats: function(){
+		that = this;
+		if (that.attacker === true) {
+				console.log("test success");
+				playerHealth = that.health;
+				playerAttack = that.attack;
+		}
+
+		if (that.defender === true) {
+			defenderHealth = that.health;
+			defenderAttack = that.attack;
+		}
+	}
+}
+
+character2 = {
+	name: "Darth Vader",
+	health: 120,
+	attack: 20,
+	Defender: false,
+	Attacker: false,
+	setStats: function(){
+		that = this;
+		if (that.attacker === true) {
+			console.log("test success");
+			playerHealth = that.health;
+			playerAttack = that.attack;
+		}
+
+		if (that.defender === true) {
+			defenderHealth = that.health;
+			defenderAttack = that.attack;
+		}
+	}
+}
+
+character3 = {
+	name: "Luke Skywalker",
+	health: 135,
+	attack: 20,
+	Defender: false,
+	Attacker: false,
+	setStats: function(){
+		that = this;
+		if (that.attacker === true) {
+			console.log("test success");
+			playerHealth = that.health;
+			playerAttack = that.attack;
+		}
+
+		if (that.defender === true) {
+			defenderHealth = that.health;
+			defenderAttack = that.attack;
+		}
+	}
+}
 
 
-$("#character-1").val(CharacterOneHealth);
-console.log($("#character-1").val())
+character4 = {
+	name: "Mace Windu",
+	health: 119,
+	attack: 20,
+	Defender: false,
+	Attacker: false,
+	setStats: function(){
+		that = this;
+		if (that.attacker === true) {
+			console.log("test success");
+			playerHealth = that.health;
+			playerAttack = that.attack;
+		}
+
+		if (that.defender === true) {
+			defenderHealth = that.health;
+			defenderAttack = that.attack;
+		}
+	}
+}
+
+var setPlayerStats = function() {
+	$(".player-health").text(playerHealth)
+}
+
+var setDefenderStats = function() {
+	$(".opponent-health").text(defenderHealth)
+	console.log(defenderHealth)
+}
+
+
+
+//$("#character-1").data("Character", {name: "obiwan", health: 130, attack: 15});
+//console.log($("#character-1").data());
 
 $(".image-placeholder").on("click", function(element){
 	clickIterator ++;
 	if (!characterChosen) {
+
+
+	//	console.log($(this).data("character").health);
+
 		$("#first-character-container").detach().appendTo("#enemies");
 		$(this).detach().appendTo("#your-character")
-		console.log($(this).attr("id"))
-		selectedAttacker = $(this).attr("id");
-		$(".player-health").html($(this).val());
+	//	selectedAttacker = $(this).data("character").name;
+	//	$(".player-health").html($(this).data("character").health);
 		characterChosen = true;
+
+
+		if ($(this).attr("id") === "character-1") {
+			character1.attacker = true;
+		}
+
+		if ($(this).attr("id") === "character-2") {
+			character2.attacker = true;
+		}
+
+		if ($(this).attr("id") === "character-3") {
+			character3.attacker = true;
+		}
+
+		if ($(this).attr("id") === "character-4") {
+			character4.attacker = true;
+		}
+
+		character1.setStats();
+		character2.setStats();
+		character3.setStats();
+		character4.setStats();
+		setPlayerStats();
+
+
+
+		//if ($(this).attr("id") ===
 	} 
 	if (clickIterator === 2) {
+
 		$(this).detach().appendTo("#enemy")
 		selectedDefender = $(this).attr("id")
+
+		if ($(this).attr("id") === "character-1") {
+			character1.defender = true;
+			console.log("test");
+		}
+
+		if ($(this).attr("id") === "character-2") {
+			character2.defender = true;
+		}
+
+		if ($(this).attr("id") === "character-3") {
+			character3.defender = true;
+		}
+
+		if ($(this).attr("id") === "character-4") {
+			character4.defender = true;
+		}
+
+		character1.setStats();
+		character2.setStats();
+		character3.setStats();
+		character4.setStats();
+		setDefenderStats();
 		
 		defenderChosen = true;
 	}
 
-console.log("selected attacker is " + selectedAttacker);
-console.log("selected defender is " + selectedDefender);
-
 })
+
+$(".fight-button").on("click", function(){
+	if (clickIterator < 2) {
+		return;
+	} else {
+	defenderHealth -= playerAttack;
+	$(".opponent-health").text(defenderHealth);
+	}
+
+	if (defenderHealth < 0) {
+		wins ++;
+		clickIterator = 1;
+		$("#enemy").empty();
+		$(".opponent-health").text("");
+		console.log(wins)
+	}
+
+	if (wins === 3) {
+		alert("You win!!!")
+	}
+})
+
 
 
