4fe70e05c8a29601a79d00e50ded50292bff399b
unknown <jtc7537@gmail.com>
2017-12-18 20:35:42 -0500

    push before changing .val to .data for character data storage

--- starwars.js
+++ starwars.js
@@ -1,58 +1,68 @@
 var characterChosen = false;
 var defenderChosen = false;
 var clickIterator = 0;
+var selectedAttacker;
+var selectedDefender;
 
-character1 = {
-	name: "Obi Wan",
-	health: 100,
-	attack: 20,
-	Defender: false,
-	Attacker: false,
-}
+//	character1 = {
+//		name: "Obi Wan",
+//		health: 100,
+//		attack: 20,
+//		Defender: false,
+//		Attacker: false,
+//	}
+//	
+//	character2 = {
+//		name: "Darth Vader",
+//		health: 100,
+//		attack: 20,
+//		Defender: false,
+//		Attacker: false,
+//	}
+//	
+//	character3 = {
+//		name: "Luke Skywalker",
+//		health: 100,
+//		attack: 20,
+//		Defender: false,
+//		Attacker: false,
+//	}
+//	
+//	
+//	character4 = {
+//		name: "Mace Windu",
+//		health: 100,
+//		attack: 20,
+//		Defender: false,
+//		Attacker: false,
+//	}
+var CharacterOneHealth = 130
+var playerHealth;
 
-character2 = {
-	name: "Darth Vader",
-	health: 100,
-	attack: 20,
-	Defender: false,
-	Attacker: false,
-}
 
-character3 = {
-	name: "Luke Skywalker",
-	health: 100,
-	attack: 20,
-	Defender: false,
-	Attacker: false,
-}
+$("#character-1").val(CharacterOneHealth);
+console.log($("#character-1").val())
 
-
-character4 = {
-	name: "Mace Windu",
-	health: 100,
-	attack: 20,
-	Defender: false,
-	Attacker: false,
-}
-
-$("#character-1-name").text(character1.name)
-$("#character-1-health").text(character1.health)
-
-$("#character-1").data(character1);
-
-console.log("test")
-
-$(".image-placeholder").on("click", function(){
+$(".image-placeholder").on("click", function(element){
 	clickIterator ++;
 	if (!characterChosen) {
 		$("#first-character-container").detach().appendTo("#enemies");
 		$(this).detach().appendTo("#your-character")
+		console.log($(this).attr("id"))
+		selectedAttacker = $(this).attr("id");
+		$(".player-health").html($(this).val());
 		characterChosen = true;
 	} 
 	if (clickIterator === 2) {
 		$(this).detach().appendTo("#enemy")
+		selectedDefender = $(this).attr("id")
 		
 		defenderChosen = true;
 	}
 
+console.log("selected attacker is " + selectedAttacker);
+console.log("selected defender is " + selectedDefender);
+
 })
+
+
